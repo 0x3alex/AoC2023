c973f001ebb2a03253038d76883846c50b6358dc
Alex <90933044+0x3alex@users.noreply.github.com>
2023-12-03 18:10:37 +0100

    Update Main.hs

--- HaskellAoC/app/Main.hs
+++ HaskellAoC/app/Main.hs
@@ -1,9 +1,8 @@
 module Main where
 import DayOne
 import DayTwo (dayTwoP1, dayTwoP2)
-import DayThree (dayThreeP1)
 -------------------------------------------
 main :: IO ()
 main = do
     s <- readFile "./app/input.txt"
-    print $ dayThreeP1 (lines s) 0 0
+    print $ dayTwoP2 $ lines s
